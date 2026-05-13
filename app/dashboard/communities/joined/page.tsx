bd73512722e3441ad3c8f65d928ce4447ec5dfca
Jesus Peralta <desarrolladorwebodm@gmail.com>
2026-05-13 15:05:54 -0600

    base display

--- app/dashboard/communities/joined/page.tsx
+++ app/dashboard/communities/joined/page.tsx
@@ -1,7 +1,10 @@
+import { membershipService } from "@/src/features/communities/services/MembershipService"
+import { requireAuth } from "@/src/lib/auth-server"
 import Heading from "@/src/shared/components/typography/Heading"
 import { generatePageTitle } from "@/src/shared/utils/metadata"
 import { Metadata } from "next"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
 const title = "Comunidades a las que te uniste"
 
@@ -9,7 +12,13 @@ export const metadata: Metadata = {
   title: generatePageTitle( title )
 }
 
-export default function JoinedCommunityPage() {
+export default async function JoinedCommunityPage() {
+
+  const { session } = await requireAuth()
+  if(!session) redirect('/auth/login')
+
+    await membershipService.getJoinedCommunities(session.user)
+
   return (
     <>
       <Heading>{ title }</Heading>
